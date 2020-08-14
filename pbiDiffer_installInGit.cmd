e54a2dc487470b09d4ed9e567ca2afac7fa07f5c
Raj Rao <rrao@ncm.com>
2020-08-14 14:26:46 -0600

    Reverting the changes to what it needs to be

--- pbiDiffer_installInGit.cmd
+++ pbiDiffer_installInGit.cmd
@@ -21,35 +21,6 @@ git config --global diff.pbix.textconv "\"%cd%\PowerBiDiffer.exe\" textconv"
 
 git config --global difftool.PowerBiDiffer.cmd "\"%cd%\PowerBiDiffer.cmd\" \"$LOCAL\" \"$REMOTE\"
 
-
-if exist "!InstallDir!" (
-	echo Using visual studio as diffTool from !InstallDir!
-
-	git config --global difftool.PowerBiDiffer.cmd "\"%cd%\PowerBiDiffer.exe\" difftool \"$LOCAL\" \"$REMOTE\" -d \"!InstallDir!\" -a '/diff \"{lp}\" \"{rp}\" \"{ln}\" \"{rn}\"'"
-	
-	echo visual studio installed as diffTool
-
-	goto SUCCESS
-)
-
-if exist "C:\Program Files (x86)\WinMerge\winmergeu.exe" (
-
-	echo Visual Studio not found, using WinMerge
-	set "winMergePath=C:\Program Files (x86)\WinMerge\winmergeu.exe"
-		
-	git config --global difftool.PowerBiDiffer.cmd "\"%cd%\PowerBiDiffer.exe\" difftool \"$LOCAL\" \"$REMOTE\" -d \"!winMergePath!\" -a '/xq /e /s /dl \"{ln}\" /dr \"{rn}\" \"{lp}\" \"{rp}\"'"
-	
-	echo WinMerge installed as diffTool
-	
-	goto SUCCESS
-)
-
-:FAILURE
-echo DiffTool was not completed successfully, as VisualStudio, nor WinMerge were found
-
-:SUCCESS
-echo Completed installing a difftool successfully!
-
 :END
 echo on
 ::pause
